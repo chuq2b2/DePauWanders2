47c290cb64b4882a57dc8db1bbf576923eccc02a
chuq2b2 <chudiemquynh195@gmail.com>
2023-04-24 21:15:27 -0400

    Fifth Commit: Improve interface with footer and interactions

--- myscripts.js
+++ myscripts.js
@@ -9,27 +9,3 @@ window.addEventListener('scroll',(e)=>{
 
   const container = document.querySelector('.container');
 const links = document.querySelector('.links');
-
-// Create anchor tags and append them to the links div
-for (let i = 0; i < container.children.length; i++) {
-  const sectionId = container.children[i].id;
-  const link = document.createElement('a');
-  link.setAttribute('href', `#${sectionId}`);
-  link.textContent = `Section ${i + 1}`;
-  links.appendChild(link);
-}
-
-// Add click event listener to each anchor tag
-links.querySelectorAll('a').forEach(link => {
-  link.addEventListener('click', e => {
-    e.preventDefault(); // Prevent default anchor tag behavior
-    // Remove active class from all anchor tags
-    links.querySelectorAll('a').forEach(link => link.classList.remove('active'));
-    // Add active class to clicked anchor tag
-    link.classList.add('active');
-    // Get target div ID from href attribute
-    const targetId = link.getAttribute('href');
-    // Scroll to target div
-    document.querySelector(targetId).scrollIntoView({ behavior: 'smooth' });
-  });
-});
