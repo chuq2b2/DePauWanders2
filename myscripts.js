63278a83b9d6ce70e7296b301a66ffb662350a72
Minh Nguyen <minhnguyen150403@gmail.com>
2023-04-28 01:42:23 -0400

    Minh 5th: Complete the fake back end using csv. Complete js for wrapped. Now just need to import data to csv file to display it in the timelime

--- myscripts.js
+++ myscripts.js
@@ -9,3 +9,55 @@ window.addEventListener('scroll',(e)=>{
 
 const container = document.querySelector('.container');
 const links = document.querySelector('.links');
+
+//-------------------------------------------------------------//
+
+d3.csv("timeLine/timeline_data.csv").then(function(data) {
+  var timeLine = {};
+  data.forEach(function(d) {
+      var date = d.Date;
+      var title = d.Title;
+      var descrip = d.Description;
+      var img = d.Img;
+
+      timeLine[date] = {'title':title, 'descrip': descrip, 'img': img};
+  });
+
+  console.log(timeLine);
+
+  
+  const keyDate = Object.keys(timeLine);
+
+  keyDate.sort(function(a,b){
+      const dateA = new Date(a);
+      const dateB = new Date(b);
+      return dateA-dateB;
+  });
+
+  console.log(keyDate);
+
+  var lr = "left";
+  var event = document.getElementsByClassName("timeline")[0];
+  keyDate.forEach(function(date){
+    var title = timeLine[date]["title"];  
+    var des = timeLine[date]["descrip"];
+    var img = timeLine[date]["img"];
+  
+    event.innerHTML += `<div class ="timeline-container ${lr}-container">
+                          <img src = "assets/pictures/organizations/${img}"/>
+                          <div class="text-box">
+                            <h2>${title} </h2>
+                            <small>${date} </small> 
+                            <p>${des}</p>
+                            <span class="${lr}-container-arrow"></span>
+                          </div>
+                        </div>
+                    `
+    if (lr == "left")
+      lr = "right";
+    else
+      lr = "left";
+                        ;
+})
+});
+
